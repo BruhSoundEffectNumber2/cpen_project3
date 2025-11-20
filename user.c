7c47996bfef1b2cac68a958a41d5d276d21cce8e
Anicolazzo <anicolazzo@zagmail.gonzaga.edu>
2025-11-20 13:41:52 -0800

    Update user.c
    Keypad implementation

--- user.c
+++ user.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include "Keypad.h"
 #include "pwmDriver.h"
-
+#include <stdlib.h>
 #define TIMESLICE 32000 // 2ms
 
 void Init_LCD_Ports(void);
@@ -25,8 +25,6 @@ int32_t I = 0;
 // Last char needs to be the null terminator
 char currentInput[5];
 
-int32_t mutex;
-int motorSpeed;
 // Add mutex
 
 void PI_Handler(void)
@@ -70,9 +68,9 @@ void SetMotorSpeed()
 {
 	while (1)
 	{
-		OS_Wait(mutex);
-		MOT34_Speed_Set(motorSpeed);
-		OS_Signal(mutex);
+		// OS_Wait(mutex);
+		// MOT34_Speed_Set(targetMotorRPM);
+		// OS_Signal(mutex);
 	}
 }
 
@@ -82,107 +80,36 @@ void InputControl()
 	unsigned char previous = ' ';
 	char dir = ' ';
 	int duty = 0;
+	int counter = 0;
 	// Read keypad input
 	while (1)
 	{
 
-		current = MatrixKeypad_Scan(&sct); // Implement Matrix scan
-		if (current != previous)
+		current = Read_Key(&sct); // Implement Matrix scan
+		if (current != 0x00)
 		{
-			switch (current)
-			{ // Switch case to select speed and output dir
-			case '0':
-				duty = 0;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				break;
-			case '1':
-				duty = 600 * 1;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				break;
-			case '2':
-				duty = 400 * 2;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				;
-				break;
-			case '3':
-				duty = 400 * 3;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				;
-				break;
-			case '4':
-				duty = 400 * 4;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				break;
-			case '5':
-				duty = 400 * 5;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				break;
-			case '6':
-				duty = 400 * 6;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				break;
-			case '7':
-				duty = 400 * 7;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				break;
-			case '8':
-				duty = 400 * 8;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				break;
-			case '9':
-				duty = 3999;
-				LCD_command(0x01);
-				LCD_data(dir);
-				LCD_command(0x06);
-				LCD_data(current);
-				break;
-			case 'E':
-				dir = '+';
-				LCD_command(0x02);
-				LCD_data(dir);
-				MOT34_Forward();
+			if ((current <= '9' || current >= '0') && counter < 4)
+			{
 
-				break;
-			case 'F':
-				dir = '-';
-				LCD_command(0x02);
-				LCD_data(dir);
-				MOT34_Reverse();
-
-				break;
-			default:
-				break;
+				currentInput[current] = current;
+				counter++;
 			}
-			OS_Wait(mutex);
-			motorSpeed = duty;
-			OS_Signal(mutex);
+			else if (current == '#')
+			{
+				counter = 0;
+				duty = atoi(currentInput);
+				OS_Wait(mutex);
+				targetMotorRPM = duty;
+				OS_Signal(mutex);
+			}
+			else if(current== 'C')
+			{
+				for(int i=0 i<4; i++)
+				{
+					currentInput[i]='\0';
+				}
+				counter=0;
+			}
 		}
 	}
 }
@@ -218,7 +145,6 @@ void LCDControl()
 
 int main(void)
 {
-	MatrixKeypad_Init();
 	uint16_t period = 4000;
 	uint16_t duty = 0; // Start at zero
 	MOT34_Init(period, duty);
@@ -239,28 +165,28 @@ int main(void)
 	Init_LCD_Ports();
 	Init_LCD();
 
-	// Enable PWM on PF2
-	GPIO_PORTF_AFSEL_R |= 0x4;
-	GPIO_PORTF_PCTL_R &= ~(0x4 << 24);
-	GPIO_PORTF_PCTL_R |= 0x4 << 24;
-	GPIO_PORTF_AMSEL_R &= ~0x4;
-	GPIO_PORTF_DEN_R |= 0x4;
+	// // Enable PWM on PF2
+	// GPIO_PORTF_AFSEL_R |= 0x4;
+	// GPIO_PORTF_PCTL_R &= ~(0x4 << 24);
+	// GPIO_PORTF_PCTL_R |= 0x4 << 24;
+	// GPIO_PORTF_AMSEL_R &= ~0x4;
+	// GPIO_PORTF_DEN_R |= 0x4;
 
-	// Configure PWM clocking
-	SYSCTL_RCC_R |= 0x1 << 20;
-	SYSCTL_RCC_R |= 0x7 << 17;
-	PWM1_3_CTL_R = 0;
-	PWM1_3_GENA_R = 0xc8;
-	PWM1_3_LOAD_R = 2499;
-	PWM1_3_CMPA_R = 0;
-	PWM1_3_CTL_R |= 1;
-	PWM1_ENABLE_R |= 0x40;
+	// // Configure PWM clocking
+	// SYSCTL_RCC_R |= 0x1 << 20;
+	// SYSCTL_RCC_R |= 0x7 << 17;
+	// PWM1_3_CTL_R = 0;
+	// PWM1_3_GENA_R = 0xc8;
+	// PWM1_3_LOAD_R = 2499;
+	// PWM1_3_CMPA_R = 0;
+	// PWM1_3_CTL_R |= 1;
+	// PWM1_ENABLE_R |= 0x40;
 
-	// TODO: Enable keypad
+	// // TODO: Enable keypad
 
-	// Initialize Port D switches (PD0-PD3) as inputs
-	GPIO_PORTD_DIR_R &= ~0x0F; // PD3-PD0 as inputs
-	GPIO_PORTD_DEN_R |= 0x0F;  // Digital enable
+	// // Initialize Port D switches (PD0-PD3) as inputs
+	// GPIO_PORTD_DIR_R &= ~0x0F; // PD3-PD0 as inputs
+	// GPIO_PORTD_DEN_R |= 0x0F;  // Digital enable
 
 	OS_AddThreads(&SetMotorSpeed, &LED_Change, &Color_Add);
 	OS_Launch(TIMESLICE); // doesn't return, interrupts enabled in here
